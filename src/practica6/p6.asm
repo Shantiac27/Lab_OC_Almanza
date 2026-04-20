04bc7a4353389dcee12573b9aaaec77c1c67ecf9
Shantiac27 <74982545+Shantiac27@users.noreply.github.com>
2026-04-20 20:45:41 +0000

    salvar cambios

--- src/practica6/p6.asm
+++ src/practica6/p6.asm
@@ -4,14 +4,103 @@
 section	.text
 	global _start       ;referencia para inicio de programa
 	
-_start:                   
-	mov edx, msg		; edx = dirección de la cadena msg
-	call puts			; imprime cadena msg terminada en valor nulo (0)
+_start:             
 
-	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
+    mov edx, ncad
+	call puts
+
+	mov bx, word[len]
+	mov edx, cad
+	call capturar
+
+    call getch
+	add al, '0'
+	call putchar
+	mov edx. ncad
+	call puts
+
 	int	0x80        	; llamada al sistema - fin de programa
 
+capturar:
+    push ecx
+    push edx
+    push ebx
+	mov cx,bx
+	dec cx
+	mov bx, 0
+
+.ciclo:
+    
+	cmp cx, 1
+	je .salir
+
+    call getch
+	cmp al, 0x7f
+	jne .guardar
+	cmp bx, 0
+	je .ciclo
+	call borrar
+	dec bx
+	jmp .ciclo
+
+.guardar:
+    cmp al, 10
+	je .salir
+	mov byte [edx + ebx], al
+	call putchar
+	inc bx
+	loop .ciclo
+
+.salir:
+    mov byte[edx + ebx],0
+	pop ebx
+	pop edx
+    pop ecx
+	ret
+
+borrar:
+    push ax 
+
+	mov al, 0x8
+	call putchar 
+
+	mov al, ' '
+	call putchar
+
+	mov al, 0x8
+	call putchar
+
+	pop ax
+	ret
+
+Mayusculas:
+    push ebx
+	push edx 
+	mov ebx , -1
+
+	.sig:
+	    inc ebx 
+		mov al, [edx + ebx]
+		cmp al, 0
+		je .salir 
+		cmp al, 'a'
+		jnb .cmpz
+		jmp .sig
+
+	.cmpz:
+	    cmp al, 'z'
+		jnb .sig
+
+		sub al, 32
+		mov byte [edx + ebx], al
+		jmp .sig
+
+	.
+
 section	.data
-    msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
+    ncad db 0xa, 'Cadena :',0
+	nlin db 0xa
+	len db 64
+	cad times 64 db 0
 
 
